ca90eff5e50e84a4551ac93c23b6272139b49727
marananramya <ramya.maranan@universumglobal.com>
2017-08-30 16:01:04 +0530

    css applied

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     content_tag :div, class: "flash-messages" do
       flash.map do |key, value|
         content_tag :div, class: "alert alert-dismissable alert-#{key}" do
-          content_tag(:span, '&times;'.html_safe, class: :close, 'data-dismiss' => 'alert') + content_tag(:p, value)
+          content_tag(:span, '&times;'.html_safe, class: :close, 'data-dismiss' => 'alert') + content_tag(:p, value, class: "danger")
         end
       end.join().html_safe
     end
